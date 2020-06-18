f7c18d16816176434a7468fb4b52c3faca426f2d
SAFNAS <iamsafnas@gmail.com>
2020-06-18 12:52:37 +0530

    created a basic app with title

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home : Text('Hey Ninjas!'),
+    home : Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+      ),
+    )
   ));
 }
