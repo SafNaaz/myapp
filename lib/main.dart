bb45033d5e267865b0e66bf95a58ca861677a904
SAFNAS <iamsafnas@gmail.com>
2020-06-18 13:17:40 +0530

    body and button basic

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,13 @@ void main() {
     home : Scaffold(
       appBar: AppBar(
         title: Text('My First App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('hello ninjas!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
       ),
     )
   ));
