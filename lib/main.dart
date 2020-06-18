6f406fbe13f1c8b1e523347df172b5d1f48b2e15
SAFNAS <iamsafnas@gmail.com>
2020-06-18 21:16:41 +0530

    expanded like flex

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home : Home()
-  ));
+  runApp(MaterialApp(home: Home()));
 }
 
 class Home extends StatelessWidget {
@@ -15,33 +13,36 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Row(
         children: <Widget>[
-        Center(
-          child: Container(
-            padding: EdgeInsets.all(30.0),
-            color: Colors.pinkAccent,
-            child:Text('one')
+          Expanded(
+            flex: 6,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.cyan,
+              child: Text('1'),
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            padding: EdgeInsets.all(20.0),
-            color: Colors.cyan,
-            child:Text('one')
+          Expanded(
+            flex: 3,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.pinkAccent,
+              child: Text('2'),
+            ),
           ),
-        ),
-        Container(
-          padding: EdgeInsets.all(40.0),
-          color: Colors.amber,
-          child:Text('one')
-        )
-      ],),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.amber,
+              child: Text('3'),
+            ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () {},
         child: Text('click'),
         backgroundColor: Colors.red[600],
       ),
