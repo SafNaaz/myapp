42895a85507f90389ee3ca79b0f6be0c36091278
SAFNAS <iamsafnas@gmail.com>
2020-06-18 19:18:22 +0530

    flatbutton sample

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: RaisedButton(
+        child: FlatButton(
           onPressed: () {},
           child:Text('click me'),
           color: Colors.lightBlue,
